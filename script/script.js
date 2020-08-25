3429e03f9a344f1415f0e88444989baa5ce016f5
CHANDAN RAJ <69450631+chandan95raj@users.noreply.github.com>
2020-08-25 07:13:37 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("cntb").style.backgroundColor = "white";
+
+  
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("cntb").style.backgroundColor = "";
+}
+
+
